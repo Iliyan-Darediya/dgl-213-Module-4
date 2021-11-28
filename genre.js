ed231a4d56c037bd6820d8b1def02a4262c0bfb0
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2021-11-27 17:07:00 -0800

    Add addDiv function to genre.js

---
+++ genre.js
@@ -0,0 +1,23 @@
+let movieGenre = document.getElementById('movieGenre').value;
+let container = document.querySelector(".container")
+
+console.log(movieGenre)
+let url = `https://api.nytimes.com/svc/movies/v2/critics/all.json?&api-key=nvZZM6U8Hx6PFfbmSsbXkYnDzqj00xp6`
+//let url = `https://api.nytimes.com/svc/movies/v2/critics/A.%20O.%20Scott.json?&api-key=nvZZM6U8Hx6PFfbmSsbXkYnDzqj00xp6`
+
+fetch(url)
+    .then(response => response.json())
+    .then(data=> console.log(data.results.map(critic=>{
+        addDiv(container,critic)
+        console.log(critic)
+    })))
+
+function addDiv(container, critic){
+    container.innerHTML +=
+    `<div class='card'>
+        <img class = 'card__image' src =${critic.multimedia ? critic.multimedia.resource.src : 'poster-holder.jpg'} >
+        <h2>${critic.display_name}</h2>
+        <h4>${critic.status?`Status ${critic.status}`: "We don't know their status"}</h4>
+        <p>${critic.bio?critic.bio:"We don't have access to their bio"}</p>
+    </div>`
+}
